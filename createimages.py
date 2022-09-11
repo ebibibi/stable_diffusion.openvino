6a88b72d765fc1652aaae84b5479c56bdb68b636
Masahiko Ebisuda <ebibibi@gmail.com>
2022-09-11 22:00:58 +0900

    commit.

--- createimages.py
+++ createimages.py
@@ -5,6 +5,6 @@ import datetime
 args = sys.argv
 
 while True:
-    filename = '/tmp/output/' + datetime.datetime.now.strftime('%Y%m%d_%H%M%S') + '.png'
-    subprocess.run("demo.py --output " + filename + " --prompt " + args[0], shell=True)
+    filename = '/tmp/output/' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.png'
+    subprocess.run("/src/demo.py --output " + filename + " --prompt " + args[0], shell=True)
 
